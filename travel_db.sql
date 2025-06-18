ea2d8f43b0860640227a67031c0ae90234969059
danhchan09 <tdanh589@gmail.com>
2025-06-18 17:12:10 +0700

    feat: add database

---
+++ travel_db.sql
@@ -0,0 +1,259 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th6 18, 2025 lúc 12:09 PM
+-- Phiên bản máy phục vụ: 10.4.32-MariaDB
+-- Phiên bản PHP: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Cơ sở dữ liệu: `travel_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `destinations`
+--
+
+CREATE TABLE `destinations` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `category` enum('thangcanh','ditich','giaitri') DEFAULT NULL,
+  `image_path` varchar(255) DEFAULT NULL,
+  `rating` tinyint(4) DEFAULT NULL,
+  `province_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `destinations`
+--
+
+INSERT INTO `destinations` (`id`, `name`, `category`, `image_path`, `rating`, `province_id`) VALUES
+(2, 'Chợ Bến Thành', 'thangcanh', 'TPHCM/ChoBenThanh.jpg', 5, 1),
+(3, 'Bến Bạch Đằng', 'thangcanh', 'TPHCM/BenBachDang.png', 5, 1),
+(4, 'Đầm Sen', 'thangcanh', 'TPHCM/DamSen.png', 5, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `events`
+--
+
+CREATE TABLE `events` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `date` varchar(50) DEFAULT NULL,
+  `location` varchar(255) DEFAULT NULL,
+  `description` text DEFAULT NULL,
+  `image_path` varchar(255) DEFAULT NULL,
+  `province_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `food`
+--
+
+CREATE TABLE `food` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `image_path` varchar(255) DEFAULT NULL,
+  `province_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `food`
+--
+
+INSERT INTO `food` (`id`, `name`, `image_path`, `province_id`) VALUES
+(2, 'Bún Mắm', 'TPHCM/BunMam.jpg', 1),
+(3, 'Cơm Tấm', 'TPHCM/ComTam.jpg', 1),
+(4, 'Hủ Tiếu', 'TPHCM/HuTieu.png', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `modal_content`
+--
+
+CREATE TABLE `modal_content` (
+  `id` int(11) NOT NULL,
+  `section` enum('lichsu','vanhoa','khiHau') DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `content` text DEFAULT NULL,
+  `province_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `overview`
+--
+
+CREATE TABLE `overview` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `content` text DEFAULT NULL,
+  `image_path` varchar(255) DEFAULT NULL,
+  `province_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `provinces`
+--
+
+CREATE TABLE `provinces` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `slug` varchar(255) DEFAULT NULL,
+  `description` text DEFAULT NULL,
+  `image_path` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `provinces`
+--
+
+INSERT INTO `provinces` (`id`, `name`, `slug`, `description`, `image_path`) VALUES
+(1, 'TP.Hồ Chí Minh', '2', 'Thành phố ở miền nam', 'assets/image/provinces/saigon.jpg'),
+(3, 'Vũng Tàu', 'dsadasd', 'Vũng Tàu là 1 tỉnh đẹp', 'assets/image/provinces/vung_tau.jpg');
+
+--
+-- Chỉ mục cho các bảng đã đổ
+--
+
+--
+-- Chỉ mục cho bảng `destinations`
+--
+ALTER TABLE `destinations`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `province_id` (`province_id`);
+
+--
+-- Chỉ mục cho bảng `events`
+--
+ALTER TABLE `events`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `province_id` (`province_id`);
+
+--
+-- Chỉ mục cho bảng `food`
+--
+ALTER TABLE `food`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `province_id` (`province_id`);
+
+--
+-- Chỉ mục cho bảng `modal_content`
+--
+ALTER TABLE `modal_content`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `province_id` (`province_id`);
+
+--
+-- Chỉ mục cho bảng `overview`
+--
+ALTER TABLE `overview`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `province_id` (`province_id`);
+
+--
+-- Chỉ mục cho bảng `provinces`
+--
+ALTER TABLE `provinces`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `slug` (`slug`);
+
+--
+-- AUTO_INCREMENT cho các bảng đã đổ
+--
+
+--
+-- AUTO_INCREMENT cho bảng `destinations`
+--
+ALTER TABLE `destinations`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT cho bảng `events`
+--
+ALTER TABLE `events`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT cho bảng `food`
+--
+ALTER TABLE `food`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT cho bảng `modal_content`
+--
+ALTER TABLE `modal_content`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT cho bảng `overview`
+--
+ALTER TABLE `overview`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT cho bảng `provinces`
+--
+ALTER TABLE `provinces`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Các ràng buộc cho các bảng đã đổ
+--
+
+--
+-- Các ràng buộc cho bảng `destinations`
+--
+ALTER TABLE `destinations`
+  ADD CONSTRAINT `destinations_ibfk_1` FOREIGN KEY (`province_id`) REFERENCES `provinces` (`id`);
+
+--
+-- Các ràng buộc cho bảng `events`
+--
+ALTER TABLE `events`
+  ADD CONSTRAINT `events_ibfk_1` FOREIGN KEY (`province_id`) REFERENCES `provinces` (`id`);
+
+--
+-- Các ràng buộc cho bảng `food`
+--
+ALTER TABLE `food`
+  ADD CONSTRAINT `food_ibfk_1` FOREIGN KEY (`province_id`) REFERENCES `provinces` (`id`);
+
+--
+-- Các ràng buộc cho bảng `modal_content`
+--
+ALTER TABLE `modal_content`
+  ADD CONSTRAINT `modal_content_ibfk_1` FOREIGN KEY (`province_id`) REFERENCES `provinces` (`id`);
+
+--
+-- Các ràng buộc cho bảng `overview`
+--
+ALTER TABLE `overview`
+  ADD CONSTRAINT `overview_ibfk_1` FOREIGN KEY (`province_id`) REFERENCES `provinces` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
